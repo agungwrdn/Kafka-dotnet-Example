68b467bc3cb63d09914b442bec74b9667ff20048
Rahardian Agung Wardana <agungwar00@gmail.com>
2023-12-06 16:13:43 +0700

    Add project files.

---
+++ Contoh Kafka/Controllers/ProducerController.cs
@@ -0,0 +1,56 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Contoh_Kafka.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        private readonly IProducer<Null, string> producer;
+        public ProducerController()
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:19092", // Ganti dengan alamat dan port Kafka broker Anda
+            };
+
+            producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+        [HttpPost]
+        public async Task<ActionResult> ExamplePublisher()
+        {
+            var topic = "insert-tnt";
+            var qrlis = new[]
+            {
+                new { id_qr = "AAAA" },
+                new { id_qr = "BBBB"}
+            };
+
+            var data = new 
+            {
+                id_qr_log_header = "BLABLABLABLA",
+                qr_list = qrlis
+            };
+
+            string json = JsonConvert.SerializeObject(data);
+
+            try
+            {
+                var produceResult = await producer.ProduceAsync(topic, new Message<Null, string> { Value = json });
+
+                Console.WriteLine($"Menghasilkan pesan ke {produceResult.Topic}: {produceResult.Value}");
+
+                return Ok($"Menghasilkan pesan ke {produceResult.Topic}: {produceResult.Value}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Gagal menghasilkan pesan: {ex.Message}");
+            }
+
+            return Ok(data);
+        }
+    }
+}
---
+++ Kafka Consumer/Program.cs
@@ -0,0 +1,60 @@
+﻿using Confluent.Kafka;
+using static Confluent.Kafka.ConfigPropertyNames;
+
+namespace Contoh_Kafka
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            IProducer<Null, string> producer;
+            var conf = new ConsumerConfig
+            {
+                GroupId = "insert-tnt",
+                BootstrapServers = "localhost:19092",
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // earliest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                Acks = Acks.All
+            };
+            producer = new ProducerBuilder<Null, string>(conf).Build();
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                var topic = "insert-tnt";
+                c.Subscribe("insert-tnt");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        var cr = c.Consume(cts.Token);
+                        try
+                        {
+                            c.Commit(cr);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'. Berhasil");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            c.Seek(cr.TopicPartitionOffset);
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    c.Close();
+                }
+            }
+        }
+    }
+}
